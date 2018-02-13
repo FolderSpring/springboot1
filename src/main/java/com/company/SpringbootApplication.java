5782eee08893d04323419f6219b2f041ee9066fd
Yehuda <yehudabukhantsov@gmail.com>
2018-02-13 12:00:00 +0200

    Add changes for deploying to WildFly

--- src/main/java/com/company/SpringbootApplication.java
+++ src/main/java/com/company/SpringbootApplication.java
@@ -2,13 +2,14 @@ package com.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Application returns image according to a search term.
  * The image can be returned from 2 places: Database / pixabay API
  */
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
